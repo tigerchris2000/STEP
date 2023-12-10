2c17e0c2435673ca749e414272501095d93ae25a
tigerchris2000 <tigerchris2000@gmail.com>
2023-12-10 14:52:50 +0100

    First push

---
+++ .modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/christopher/Desktop/STEP/copy/task2/modules.order := {   echo /home/christopher/Desktop/STEP/copy/task2/usbtemp.ko; :; } > /home/christopher/Desktop/STEP/copy/task2/modules.order
---
+++ .usbtemp.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/christopher/Desktop/STEP/copy/task2/usbtemp.ko := ld -r -m elf_x86_64 -z noexecstack --build-id=sha1  -T scripts/module.lds -o /home/christopher/Desktop/STEP/copy/task2/usbtemp.ko /home/christopher/Desktop/STEP/copy/task2/usbtemp.o /home/christopher/Desktop/STEP/copy/task2/usbtemp.mod.o;  true
