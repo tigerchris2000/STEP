0e119352b6b6354b490be3748512c00edd6e12c3
tigerchris2000 <tigerchris2000@gmail.com>
2023-12-12 21:39:52 +0100

    now changeable time for user interface

--- tempeture_reader.py
+++ tempeture_reader.py
@@ -1,23 +1,23 @@
 import sys
 import time
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print("Input should be:")
-    print("[location of usb device] [probe to call]")
+    print("[location of usb device] [probe to call] [duration in s]")
     SystemExit()
 
 
 file_location = sys.argv[1] + "probe" + sys.argv[2]
-data = open("data.txt","w")
+data = open("data.txt", "w")
 
 tracker = 0
 
-for i in range(10):
+for i in range(int(sys.argv[3])):
     file = open(file_location, "r")
     line = file.readline()
     time.sleep(0.5)
     tracker = tracker + 0.5
-    data.write(str(tracker) + " " + line);
+    data.write(str(tracker) + " " + line)
     file.close()
 
 data.close()
